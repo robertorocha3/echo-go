9d4aaa088c165db0304657355fe28bb453b6e301
robertorocha3 <robertorocha3@gmail.com>
2021-01-13 16:33:11 +1300

    Update build-properties.sh

--- build-properties.sh
+++ build-properties.sh
@@ -11,11 +11,6 @@ echo
 
 export APP_NAME="echo-go"
 export GO_MODULE="roberto.local/echo"
-
-#export REPO="robertorocha" # docker hub
-#export REPO="nexus-devops.tigersoftware.local:5003" # local repo
-#export REPO="sonatype-nexus-service.devops.svc.cluster.local:5003" # local repo
-
 export APP_VERSION="1.0"
 export PORT=3000
 export ENDPOINT="/echo"
